dc9655a31bc31fb95d0b4f0f62d96720c5f8d643
cinnamonbun1233 <cinnamonbun1233@yandex.ru>
2023-02-16 09:45:03 +0300

    update

---
+++ src/main/java/org/example/MainWindow.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320, 345);
+        setLocation(400, 400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+    }
+}
